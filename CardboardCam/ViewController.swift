5050e730f4d3a26f94f52cfcf8ecb0116914f4af
Jens Woltering <Jens.woltering@haw-hamburg.de>
2015-05-21 20:04:55 +0200

    initial commit

---
+++ CardboardCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardboardCam
+//
+//  Created by Jens Woltering on 21.05.15.
+//  Copyright (c) 2015 Jens Woltering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
