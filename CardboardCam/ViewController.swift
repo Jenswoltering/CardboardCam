17dc14f2897d4ef6c18ae09fab5060219f9c797a
Jens Woltering <Jens.woltering@haw-hamburg.de>
2015-05-21 21:18:22 +0200

    added multiplier connectivity classes

--- CardboardCam/ViewController.swift
+++ CardboardCam/ViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,MCBrowserViewControllerDelegate {
+    
+    var appDelegate:AppDelegate!
+    
+    @IBAction func connectWithPlayer(sender: AnyObject) {
+        if appDelegate.mpcHandler.session != nil{
+            appDelegate.mpcHandler.setupBrowser()
+            appDelegate.mpcHandler.browser.delegate = self
+            
+            self.presentViewController(appDelegate.mpcHandler.browser, animated: true, completion: nil)
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.mpcHandler.setupPeerWithDisplayName(UIDevice.currentDevice().name)
+        appDelegate.mpcHandler.setupSession()
+        appDelegate.mpcHandler.advertiseSelf(true)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "peerChangedStateWithNotification:", name: "MPC_DidChangeStateNotification", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleReceivedDataWithNotification:", name: "MPC_DidReceiveDataNotification", object: nil)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func peerChangedStateWithNotification(notification:NSNotification){
+        let userInfo = NSDictionary(dictionary: notification.userInfo!)
+        
+        let state = userInfo.objectForKey("state") as! Int
+        
+        if state != MCSessionState.Connected.rawValue{
+            self.navigationItem.title = "Connected"
+        }
+        if state != MCSessionState.NotConnected.rawValue{
+            self.navigationItem.title = "Not Connected"
+        }
+        if state != MCSessionState.Connecting.rawValue{
+            self.navigationItem.title = "Connecting"
+        }
+    }
+    
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
 }
 
