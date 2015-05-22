55e9b45a4f1735a082f07a33b5e09f4c927caee6
Jens Woltering <Jens.woltering@haw-hamburg.de>
2015-05-22 13:02:13 +0200

    initScreen added

---
+++ CardboardCam/initViewController.swift
@@ -0,0 +1,61 @@
+//
+//  initViewController.swift
+//  CardboardCam
+//
+//  Created by Jens Woltering on 22.05.15.
+//  Copyright (c) 2015 Jens Woltering. All rights reserved.
+//
+
+import UIKit
+
+class initViewController: UIViewController {
+
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var counter:Int = 0 {
+        didSet {
+            let fractionalProgress = Float(counter) / 100.0
+            let animated = counter != 0
+            
+            progressBar.setProgress(fractionalProgress, animated: animated)
+            progressLabel.text = ("\(counter)%")
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        progressBar.setProgress(0, animated: true)
+        progressLabel.text = "0%"
+        self.counter = 0
+        for i in 0..<100 {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {
+                sleep(1)
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.counter++
+                    return
+                })
+            })
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
